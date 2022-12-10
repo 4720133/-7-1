f4792b46826dad409161378e709a9c0ce34b9074
4720133 <4720133h@hgu.jp>
2022-12-10 23:05:34 +0900

    ブログ投稿詳細画面実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,8 +15,8 @@
         <div class='posts'>
              @foreach ($posts as $post)
             <div class='post'>
-                <h2class='title'>
-                    {{ $post->title }}
+                <h2 class='title'>
+                    <a href="/posts/{{ $post->id}}">{{ $post->title }}</a>
                 </h2>
                 <p class='body'>
                     {{ $post->body}}
